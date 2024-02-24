4f5c17ab4dad5d114812be54b38c08d1d8322ee1
Luiz Gustavo Marques <62360761+cmarquesluizgustavo@users.noreply.github.com>
2024-02-24 20:16:32 -0300

    added modularity to the similarity algorithm, fixes

--- network_analysis.py
+++ network_analysis.py
@@ -1,9 +1,10 @@
 import pickle
+from re import sub
 from typing import Mapping
-from networkx import jaccard_coefficient
 import numpy as np
 import pandas as pd
 import networkx as nx
+from matplotlib import pyplot as plt
 
 
 class BasicStatistics:
@@ -63,22 +64,52 @@ class BasicStatistics:
             return nx.diameter(self.g.subgraph(self.largest_cc))
 
 
+def is_clique(G):
+    """
+    This function returns if a graph is a clique.
+    """
+    n = len(G)
+    return G.number_of_edges() == n * (n - 1) / 2
+
+
 class featuresGains:
     """
     This class is a wrapper to calculate the gain in similarity of each
-    features in a graph. It uses the Jaccard coefficient to measure the
+    features in a graph. It uses a similarity coefficient to measure the
     influence of each features in the similarity of a node.
     """
 
-    def __init__(self, G: nx.Graph) -> None:
+    def __init__(
+        self, G: nx.Graph, target_feature: list, similarity_algorithm: str = "jaccard"
+    ):
         self.g = G
+        self.target_feature = target_feature
+        self.similarity_algorithm = similarity_algorithm
 
-        self.nodes_jaccards = pd.DataFrame()
-        base_jaccard = self.avg_jaccard_4_nodes_by_graph(self.g)
-        # self.nodes_jaccards["Base"] = base_jaccard.values()
-        # self.
-        # self.gains = {}
-        # self.avg_jaccard["all"] = self.avg_jaccard_4_nodes_by_graph(self.g)
+        self.similarity = self.get_all_similarities()
+
+    def get_all_similarities(self) -> pd.DataFrame:
+        base_similarity = self.avg_similarity_4_nodes_by_graph(self.g)
+
+        similarity = pd.DataFrame(
+            columns=pd.MultiIndex.from_tuples(
+                [("Base", "value"), ("Base", "label"), ("Base", "gain")]
+            )
+        )
+        similarity["Base", "value"] = pd.Series(base_similarity).to_frame("Base")
+        similarity["Base", "label"] = "Base"
+        similarity["Base", "gain"] = 0
+
+        for feature in self.target_feature:
+            feature_df = self.get_similarity_4_feature(feature)
+            feature_df[feature, "gain"] = (
+                feature_df[feature, "value"] - similarity[("Base", "value")]
+            )
+            similarity = pd.merge(
+                similarity, feature_df, left_index=True, right_index=True
+            )
+
+        return similarity
 
     def get_subgraphs(self, feature: str) -> Mapping[str, nx.Graph]:
         """
@@ -88,48 +119,69 @@ class featuresGains:
         feature_values_subgraphs = {}
         for feature_value in feature_values:
             feature_values_subgraphs[feature_value] = self.g.subgraph(
-                [n for n, d in self.g.nodes(data=True) if d[feature] == feature_value]
+                [
+                    n
+                    for n, d in self.g.nodes(data=True)
+                    if feature in d and d[feature] == feature_value
+                ]
             )
         return feature_values_subgraphs
 
-    def get_avg_jaccard(self, node, subgraph: nx.Graph) -> float:
+    def avg_similarity_4_nodes_by_graph(self, subgraph: nx.Graph) -> dict[int, float]:
         """
-        This function returns the average Jaccard coefficient of a node in a subgraph.
+        This function calculates the average similarity of a node in a subgraph.
         """
-        neighbors = set(subgraph.neighbors(node))
-        if len(neighbors) == 0:
-            return 0.0
+        # If the subgraph is connected, the average similarity is 1
+        if is_clique(subgraph):
+            return {k: 1 for k in subgraph.nodes()}
+        if self.similarity_algorithm == "jaccard":
+            similarities = nx.jaccard_coefficient(subgraph)
+        else:
+            raise ValueError("Similarity algorithm not implemented.")
+        avg_similarity, node_neighbors = {}, {}
 
-        jaccard_coefficients = nx.jaccard_coefficient(
-            subgraph, [(node, n) for n in neighbors]
-        )
+        for u, v, similarity in similarities:
+            if similarity != 0:
+                avg_similarity[u] = avg_similarity.get(u, 0) + similarity
+                avg_similarity[v] = avg_similarity.get(v, 0) + similarity
+                node_neighbors[u] = node_neighbors.get(u, 0) + 1
+                node_neighbors[v] = node_neighbors.get(v, 0) + 1
 
-        avg_jaccard = 0
-        for u, v, jaccard in jaccard_coefficients:
-            avg_jaccard += jaccard
-        return avg_jaccard / len(neighbors)
+        avg_similarity = {
+            node: avg_similarity[node] / node_neighbors[node] for node in avg_similarity
+        }
 
-    def avg_jaccard_4_nodes_by_graph(self, subgraph: nx.Graph) -> dict[int, float]:
-        """
-        This function calculates the average Jaccard coefficient for each node in a subgraph.
-        """
-        avg_jaccard = {}
-        for node in subgraph.nodes:
-            avg_jaccard[node] = self.get_avg_jaccard(node, subgraph)
-        return avg_jaccard
+        return avg_similarity
 
-    def get_jaccard_4_feature(self, feature: str) -> dict:
+    def get_similarity_4_feature(self, feature: str) -> pd.DataFrame:
         """
-        This function returns the average Jaccard coefficient for each feature.
+        This function returns the average similarity coefficient for each feature value.
         """
         feature_values_subgraphs = self.get_subgraphs(feature)
+        feature_similarity_df = pd.DataFrame()
+
         for feature_value, subgraph in feature_values_subgraphs.items():
-            self.nodes_jaccards[feature_value] = self.avg_jaccard_4_nodes_by_graph(
-                subgraph
-            ).values()
+            avg_similarity = self.avg_similarity_4_nodes_by_graph(subgraph)
 
+            feature_similarity_df_inter = pd.DataFrame(
+                {
+                    feature: feature_value,
+                    "value": list(avg_similarity.values()),
+                },
+                index=list(avg_similarity.keys()),
+            )
 
-self = featuresGains(G)
+            if feature_similarity_df_inter.empty:
+                continue
+            feature_similarity_df = pd.concat(
+                [feature_similarity_df, feature_similarity_df_inter]
+            )
+
+        feature_similarity_df.columns = pd.MultiIndex.from_tuples(
+            [(feature, "label"), (feature, "value")]
+        )
+
+        return feature_similarity_df
 
 
 def CCDF(data: list[int | float]) -> np.ndarray:
@@ -147,4 +199,10 @@ def CCDF(data: list[int | float]) -> np.ndarray:
 
 if __name__ == "__main__":
     G = pickle.load(open("data/networks/2023.pkl", "rb"))
-    getBasicStatistics(G)
+    self = featuresGains(
+        G,
+        target_feature=[
+            "siglaPartido",
+            "siglaUf",
+        ],
+    )
